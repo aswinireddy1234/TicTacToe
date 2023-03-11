f2e6e5aa3c53a5d639cb52632f0f9faee6f25517
“aswinireddy1234” <chegireddyaswinireddy@gmail>
2023-03-11 14:10:58 +0530

    ADDED: Choose a letter 0 or x

--- UC1-Create_TicTacToe.java
+++ UC1-Create_TicTacToe.java
@@ -8,15 +8,12 @@ public class TicTacToe {
             board[i] = ' ';
             System.out.println(board);
         }
-    }
-
-    public static void main(String[] args) {
+    }public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe ");
         System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
         System.out.println("---------");
         System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
         System.out.println("---------");
-
         System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
         startGame();
     }
